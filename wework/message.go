f8e4d9b90b0937ee98a43b057f15500910a4e497
xuthus5 <xuthus5@gmail.com>
2020-11-28 23:09:19 +0800

    fix

--- wework/message.go
+++ wework/message.go
@@ -63,6 +63,7 @@ func (m *Message) SendText2User() (*MessageSendResponse, error) {
 	log.Infof("send url: %v", url)
 	var rsp MessageSendResponse
 	log.Infof("send text: %v", m.Text)
+	log.Infof("send to user: %v", m.Text.Touser)
 	err = gout.POST(url).Debug(true).SetBody(m.Text).BindJSON(&rsp).Do()
 	if err != nil {
 		log.Errorf("err: %v", err)
