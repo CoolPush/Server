d12d0f6c837547a30619e4279a78decfaf1f6d11
xuthus5 <xuthus5@gmail.com>
2020-11-28 22:28:17 +0800

    fix

--- core.go
+++ core.go
@@ -1232,6 +1232,8 @@ func WwSend(w http.ResponseWriter, r *http.Request, p httprouter.Params) {
 		return
 	}
 
+	log.Infof("get skey: %v", skey)
+
 	// 检测是否有记录绑定
 	user, exist, err := SearchBySkeyBindWework(skey)
 	if err != nil {
